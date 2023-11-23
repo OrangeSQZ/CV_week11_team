02431dcdc626396f3b8670d24c14f5d867377932
OrangeSQZ <max5131004@gmail.com>
2023-11-23 12:15:17 +0900

    test 2

--- web/app.py
+++ web/app.py
@@ -1,11 +1,58 @@
 from flask import Flask, render_template
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from io import BytesIO
+import base64
 
 app = Flask(__name__)
 
+# CSV 파일 읽기
+file_path = "../data.csv"
+df = pd.read_csv(file_path)
+
+# 함수: 20대 남성 중에서 폭력적인 재범 발생여부가 제일 높은 인종
+def plot_violent_recid_by_race():
+    young_adult_males = df[(df['age'] >= 20) & (df['age'] < 30) & (df['sex'] == 'Male')]
+    violent_recid_by_race = young_adult_males.groupby('race')['is_violent_recid'].mean().sort_values(ascending=False)
+
+    plt.figure(figsize=(8, 4))
+    sns.barplot(x=violent_recid_by_race.index, y=violent_recid_by_race.values, palette='viridis')
+    plt.title('Probability of violent recidivism (male in his 20s)')
+    plt.xlabel('Race')
+    plt.ylabel('Average probability of recidivism')
+    plt.xticks(rotation=45)
+    img = BytesIO()
+    plt.savefig(img, format='png')
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode()
+    return plot_url
+
+# 함수: 아시아 여성 중에서 일반적 재범 발생여부가 제일 높은 나이대
+def plot_recid_by_age_group():
+    asian_females = df[(df['race'] == 'Asian') & (df['sex'] == 'Female')]
+    asian_females['age_group'] = pd.cut(asian_females['age'], bins=range(10, 101, 10), labels=[f'{i}~{i+9}' for i in range(10, 100, 10)])
+    recid_by_age_group = asian_females.groupby('age_group')['is_recid'].mean().sort_values(ascending=False)
+
+    plt.figure(figsize=(8, 4))
+    sns.barplot(x=recid_by_age_group.index, y=recid_by_age_group.values, palette='viridis')
+    plt.title('Typical probability of recidivism (Asian women)')
+    plt.xlabel('Age Group')
+    plt.ylabel('Average probability of recidivism')
+    plt.xticks(rotation=45)
+    img = BytesIO()
+    plt.savefig(img, format='png')
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode()
+    return plot_url
+
 @app.route('/')
 def index():
-    # Render the main page
-    return render_template('index.html', data=your_analyzed_data)
+    # 두 개의 그래프 이미지를 전달
+    plot1_url = plot_violent_recid_by_race()
+    plot2_url = plot_recid_by_age_group()
+    return render_template('index.html', plot1_url=plot1_url, plot2_url=plot2_url)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=False)
+
